87699a43a0b7b4892bcf1fcc6fb82edc6ca3c985
Nadja Probst <nadja.schwarzenegger@gmail.com>
2024-07-11 14:20:43 +0200

    basic model and controller build for raptor route, work in progress

--- src/controllers/raptorsController.js
+++ src/controllers/raptorsController.js
@@ -1,1 +1,10 @@
+import { Raptor } from "../models/raptorModel.js";
 
+export const getAllRaptors = async (req, res, next) => {
+  try {
+    const raptors = await Raptor.find();
+    res.status(200).json(raptors);
+  } catch (error) {
+    next(error);
+  }
+};
--- src/routers/raptorsRouter.js
+++ src/routers/raptorsRouter.js
@@ -1,18 +1,15 @@
 import express from "express";
 
 // TODO: Import the functions from the controller file
+import { getAllRaptors } from "../controllers/raptorsController.js";
 
 export const raptorsRouter = express.Router();
 
-/* raptorsRouter
-  .route("/")
-  .get(getAllRaptors)
-  .post(addRaptor)
-  .delete(deleteAllRaptors);
+raptorsRouter.route("/").get(getAllRaptors);
+/*  .post(addRaptor)
+  .delete(deleteAllRaptors); */
 
-raptorsRouter
-  .route("/:id")
-  .get(getRaptorById)
+raptorsRouter.route("/:id");
+/*   .get(getRaptorById)
   .patch(updateRaptorById)
-  .delete(deleteRaptorById);
- */
+  .delete(deleteRaptorById); */

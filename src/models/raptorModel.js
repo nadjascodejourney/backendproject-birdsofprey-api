2c93fac27af3aa96bd85b42cdca0a9c13726b669
Nadja Probst <nadja.schwarzenegger@gmail.com>
2024-07-15 10:14:07 +0200

    raptormodel ready for now

--- src/models/raptorModel.js
+++ src/models/raptorModel.js
@@ -7,47 +7,65 @@ const raptorSchema = new mongoose.Schema(
       lowercase: true, // lowercase: boolean, whether to always call .toLowerCase() on the value
       required: true,
       unique: true,
+      trim: true,
     },
     scientific_name: {
       type: String, // type is a special property in Mongoose schemas. When Mongoose finds a nested property named type in your schema, Mongoose assumes that it needs to define a SchemaType with the given type; type: { type: String } would be a workaround to store something as an object with a type property
       lowercase: true,
       required: true,
       unique: true,
+      trim: true,
     },
     family: {
       type: String,
       lowercase: true,
       required: true,
+      trim: true,
     },
     genus: {
       type: String,
       lowercase: true,
       required: true,
+      trim: true,
     },
     nesting_habitat: {
-      type: [String],
+      type: [String], // [String] is an array of strings; every value will be added to the array
       lowercase: true,
+      trim: true,
     },
     prey_type: {
       type: [String],
       lowercase: true,
+      trim: true,
     },
     continents: {
       type: [String],
       lowercase: true,
       required: true,
+      trim: true,
     },
     conservation_status: {
       type: String,
       lowercase: true,
+      required: true,
+      enum: [
+        "data deficient",
+        "least concern",
+        "near threatened",
+        "vulnerable",
+        "endangered",
+        "critically endangered",
+      ], // IUCN Red List categories
     },
     habitat_type: {
       type: [String],
       lowercase: true,
+      trim: true,
     },
     associated_falconries: {
       type: [String],
       lowercase: true,
+      trim: true,
     },
     wing_span_cm: {
       type: Number,
